8c4062cb268f7425699f1f3ed5747927aa30fd3f
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2024-03-18 18:21:00 +0000

    Updated CSS for a cleaner look

--- home/views.py
+++ home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from blog.models import BlogPost
 
 def home_view(request):
-    return render(request, 'home/home.html')
+    latest_blog_posts = BlogPost.objects.order_by('-created_at')[:3]
+
+    context = {
+        'latest_blog_posts': latest_blog_posts,
+    }
+    return render(request, 'home/home.html', context)
