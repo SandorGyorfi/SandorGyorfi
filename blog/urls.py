99aae632800098af4db42fa690ca160622c0dd22
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2024-03-23 20:42:15 +0000

    Fixes the blog post look on home page, closes #issue_number

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import BlogPostListView, BlogPostDetailView
 
+from. import views
+
 urlpatterns = [
     path('', BlogPostListView.as_view(), name='blogpost_list'),
-    path('<int:pk>/', BlogPostDetailView.as_view(), name='blogpost_detail'),
+    path('blog/', views.BlogPostListView.as_view(), name='blog_list'),
+    path('blog/<int:pk>/', views.BlogPostDetailView.as_view(), name='blog_detail'),
 ]
